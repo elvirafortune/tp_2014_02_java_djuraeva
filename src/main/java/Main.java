54380fddd6eb644c9a3e98dbc668305e51eaf6da
kolesofortuni <kolesofortuni@mail.ru>
2014-02-22 00:21:40 +0400

    msg
    msg

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Elvira on 21.02.14.
+ */
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.Servlet;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        Servlet frontend = new Frontend1();
+
+        Server server = new Server(8080);
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.addServlet(new ServletHolder(frontend), "/*");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(false);
+        resourceHandler.setResourceBase("static");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resourceHandler, servletContextHandler});
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+}
